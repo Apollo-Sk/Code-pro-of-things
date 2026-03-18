d8429da033a9e114e0ed98b5d7bfd6c74588a03b
Apollo-Sk <rarethon@gmail.com>
2026-03-18 15:30:34 +1100

    new change in listing

--- listing.py
+++ listing.py
@@ -13,4 +13,15 @@ print(match_words(["abc","21","long","tat","cbc"]))
 
 
 
-        
+n = 5
+
+for i in range(n):
+    for j in range(i):
+        print("* ", end="")
+    print('')
+    
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print("* ", end="")
+    print('')
